0136c719f57752b8f401d2594ac91ed3c3521468
pedronarbondo <pedro.narbondo@hotmail.com>
2022-08-24 15:56:25 -0300

    Make bottomside respond to api call

--- src/components/Data/daysData.js
+++ src/components/Data/daysData.js
@@ -1,39 +1,65 @@
 import React from "react"
 import { useState, useEffect } from "react"
 
-async function getCoordinates(city) {
-    try {
-        let coordinates = {}
-        const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=99f47c30a36a2928fc73121f9434dc1c`)
-        const data = await response.json()
-        coordinates = { lat: data[0].lat, lon: data[0].lon }
-        return coordinates
-    }
-    catch {
-        console.log("Couldn't find your city")
-    }
+export default function DaysData() {
+    const [currentCity, setCurrentCity] = useState("montevideo")
+    const [daysInfo, setDaysInfo] = useState([])
+    const [currentDayInfo, setCurrentDayInfo] = useState([]) 
+
+    useEffect(() => {
+        async function getCoordinates(city) {
+            try {
+                let coordinates = {}
+                const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=99f47c30a36a2928fc73121f9434dc1c`)
+                const data = await response.json()
+                coordinates = { lat: data[0].lat, lon: data[0].lon }
+                return coordinates
+            }
+            catch {
+                console.log("Couldn't find your city")
+            }
+        }
+
+        async function getWeatherData(coordinates) {
+            try {
+                let weatherData = []
+                const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&units=${"metric"}&appid=99f47c30a36a2928fc73121f9434dc1c`)
+                const data = await response.json()
+                weatherData = data
+                return weatherData
+            }
+            catch {
+                console.log("Couldn't find your city")
+            }
+        }
+
+        async function returnData() {
+            let coordinates = await getCoordinates(currentCity)
+            let weatherInfo = await getWeatherData(coordinates)
+            console.log(weatherInfo)
+            weatherInfo.daily.map((day, index) => {
+                setDaysInfo(prevInfo => {
+                    return [
+                        ...prevInfo,
+                        {
+                            key: index,
+                            id: index,
+                            day: new Date(day.dt*1000).toLocaleDateString("en-US", { weekday: "long"}),
+                            average_temp: Math.floor((day.temp.min + day.temp.max) / 2),
+                            min_temp: Math.floor(day.temp.min),
+                            max_temp: Math.floor(day.temp.max),
+                            cloudiness: day.clouds
+                        }]
+                })
+            })
+
+        }
+        returnData()
+    }, [currentCity])
+
+    return daysInfo
 }
 
-async function getWeatherData(coordinates) {
-    try {
-        let weatherData = []
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=99f47c30a36a2928fc73121f9434dc1c`)
-        const data = await response.json()
-        weatherData = data
-        console.log(weatherData)
-        return weatherData
-    }
-    catch {
-        console.log("Couldn't find your city")
-    }
-}
-
-async function returnData() {
-    let coordinates = await getCoordinates("montevideo")
-    getWeatherData(coordinates)
-}
-
-returnData()
 
 const daysInfo = [
     {
--- src/components/Days.js
+++ src/components/Days.js
@@ -1,9 +1,10 @@
 import React from "react"
-import { daysInfo } from "./Data/daysData"
+import daysInfoState, { daysInfo } from "./Data/daysData"
 import sunnyDay from "../assets/icons/sunnyDay.svg"
+import cloudyDay from "../assets/icons/cloudyDay.svg"
 import "./Days.css"
 
-export default function Days(props) {
+/*export default function Days() {
     const mappedDays = daysInfo.map(day => {
         return (
             <div key={day.id} className="dayInfo">
@@ -11,6 +12,28 @@ export default function Days(props) {
                 <h3>{day.averageTemp}</h3>
                 <h5>{day.minTemp} | {day.highTemp}</h5>
                 <img src={sunnyDay} />
+            </div>
+        )
+    })
+
+    console.log(daysInfoState())
+    return (
+        <div className="dayInfoHolder">
+            {mappedDays}
+        </div>
+    )
+} */
+
+export default function Days() {
+    const dayInfo = daysInfoState()
+    console.log(dayInfo)
+    const mappedDays = dayInfo.map(day => {
+        return (
+            <div key={day.id} className="dayInfo">
+                <h4>{day.day}</h4>
+                <h3>{day.average_temp} °C</h3>
+                <h5>{day.min_temp} °C | {day.max_temp} °C</h5>
+                <img src={day.cloudiness > 50 ? cloudyDay : sunnyDay}/>
             </div>
         )
     })
