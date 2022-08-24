8cc2877ce479c1f7bd4ec9a248b16a6e6b8b669d
pedronarbondo <pedro.narbondo@hotmail.com>
2022-08-24 14:30:31 -0300

    Add coordinate parser, and api caller function

--- src/components/Data/daysData.js
+++ src/components/Data/daysData.js
@@ -1,3 +1,40 @@
+import React from "react"
+import { useState, useEffect } from "react"
+
+async function getCoordinates(city) {
+    try {
+        let coordinates = {}
+        const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=99f47c30a36a2928fc73121f9434dc1c`)
+        const data = await response.json()
+        coordinates = { lat: data[0].lat, lon: data[0].lon }
+        return coordinates
+    }
+    catch {
+        console.log("Couldn't find your city")
+    }
+}
+
+async function getWeatherData(coordinates) {
+    try {
+        let weatherData = []
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=99f47c30a36a2928fc73121f9434dc1c`)
+        const data = await response.json()
+        weatherData = data
+        console.log(weatherData)
+        return weatherData
+    }
+    catch {
+        console.log("Couldn't find your city")
+    }
+}
+
+async function returnData() {
+    let coordinates = await getCoordinates("montevideo")
+    getWeatherData(coordinates)
+}
+
+returnData()
+
 const daysInfo = [
     {
         id: 0,
